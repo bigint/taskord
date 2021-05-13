e00b9faedaffaaeeed8d827636da5839013fa832
Yogi <yoginth@hey.com>
2021-05-13 18:15:54 +0530

    Autofill data

--- app/Http/Livewire/Onboarding/Profile.php
+++ app/Http/Livewire/Onboarding/Profile.php
@@ -7,10 +7,18 @@ use Livewire\Component;
 class Profile extends Component
 {
     public $user;
+    public $firstname;
+    public $lastname;
+    public $username;
+    public $bio;
 
     public function mount($user)
     {
         $this->user = $user;
+        $this->firstname = $user->firstname;
+        $this->lastname = $user->lastname;
+        $this->username = $user->username;
+        $this->bio = $user->bio;
     }
 
     public function render()
--- resources/views/livewire/onboarding/profile.blade.php
+++ resources/views/livewire/onboarding/profile.blade.php
@@ -1,22 +1,22 @@
 <div>
-    <form>
+    <form wire:target="submit" wire:submit.prevent="submit">
         <div class="row">
             <div class="col-md-6">
                 <label class="form-label fw-bold">Firstname</label>
-                <input type="text" class="form-control" placeholder="John">
+                <input type="text" class="form-control" placeholder="John" wire:model.defer="firstname">
             </div>
             <div class="col-md-6">
                 <label class="form-label fw-bold">Lastname</label>
-                <input type="text" class="form-control" placeholder="Doe">
+                <input type="text" class="form-control" placeholder="Doe" wire:model.defer="lastname">
             </div>
         </div>
         <div class="mt-3">
             <label class="form-label fw-bold">Username</label>
-            <input type="text" class="form-control" placeholder="johndoe">
+            <input type="text" class="form-control" placeholder="johndoe" wire:model.defer="username">
         </div>
         <div class="mt-3">
             <label class="form-label fw-bold">Bio</label>
-            <textarea class="form-control" rows="4" placeholder="Bio"></textarea>
+            <textarea class="form-control" rows="4" placeholder="Bio" wire:model.defer="bio"></textarea>
         </div>
         <div class="text-center">
             <button type="submit" class="btn btn-primary px-5 mt-4 fw-bold">
