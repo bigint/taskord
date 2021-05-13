73630d8177c4a759625f5be53232061c3fa4537c
Yogi <yoginth@hey.com>
2021-05-13 13:37:35 +0530

    Add onboard status column

--- database/migrations/2021_05_13_080250_add_onboard_status_to_users_table.php
+++ database/migrations/2021_05_13_080250_add_onboard_status_to_users_table.php
@@ -14,7 +14,7 @@ class AddOnboardStatusToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('onboard_status')->default('new');
         });
     }
 
@@ -26,7 +26,7 @@ class AddOnboardStatusToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('onboard_status');
         });
     }
 }
