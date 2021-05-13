118b69d8c85d2f94afd0e30791190a0c2a797377
Yogi <yoginth@hey.com>
2021-05-13 17:01:34 +0530

    Add bust emoji

--- resources/views/onboarding/profile.blade.php
+++ resources/views/onboarding/profile.blade.php
@@ -9,12 +9,12 @@
 @section('content')
 <div class="container col-lg-5">
     <div class="text-center my-5">
-        <img class="mb-3" src="https://ik.imagekit.io/taskordimg/wave_loUaex5EE.png" height="70" alt="Taskord Logo">
-        <div class="h1">Welcome to Taskord</div>
+        <img class="mb-3" src="https://ik.imagekit.io/taskordimg/bust_76KPFmEAu.png" height="70" alt="Taskord Logo">
+        <div class="h1">Setup your profile!</div>
         <div class="mt-3 h4 lh-sm text-secondary fw-normal" style="letter-spacing:1px">
             Thanks for signing up to Taskord, you are going to love the community and awesome makers here.
         </div>
-        <a class="btn btn-primary px-5 mt-4 fw-bold" href="">
+        <a class="btn btn-primary px-5 mt-4 fw-bold" href="{{ route('onboarding.profile') }}">
             Let's do it
         </a>
     </div>
